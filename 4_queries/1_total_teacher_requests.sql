49b86d814b873a5ce17891aad876ae55268c3308
cchand-vanc <cmchandler@live.co.uk>
2023-02-07 19:57:49 -0800

    Adds queries related to teachers and assisstance requests tables

---
+++ 4_queries/1_total_teacher_requests.sql
@@ -0,0 +1,6 @@
+SELECT name, count(assistance_requests.*) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name
+;
